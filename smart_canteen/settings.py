3dc50fd96ef22487ea3cc429e324b92786ca7101
Sharanya-katukuri <katukurisharanya@gmail.com>
2025-11-16 19:47:57 +0530

    changes

--- smart_canteen/settings.py
+++ smart_canteen/settings.py
@@ -67,7 +67,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'menu_list'
